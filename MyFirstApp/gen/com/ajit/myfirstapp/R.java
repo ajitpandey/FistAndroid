dd17f6f62f6d32e4729655e84903a12239c87d7f
ajitpandey <ajitpandey@outlook.com>
2014-02-13 22:30:14 +0000

    IMAGE

--- MyFirstApp/gen/com/ajit/myfirstapp/R.java
+++ MyFirstApp/gen/com/ajit/myfirstapp/R.java
@@ -17,34 +17,43 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int info=0x7f020001;
-        public static final int next=0x7f020002;
-        public static final int prev=0x7f020003;
-        public static final int start=0x7f020004;
+        public static final int car_blue=0x7f020000;
+        public static final int car_red=0x7f020001;
+        public static final int car_yellow=0x7f020002;
+        public static final int flower=0x7f020003;
+        public static final int flower_blue=0x7f020004;
+        public static final int flower_red=0x7f020005;
+        public static final int flower_yellow=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int info=0x7f020008;
+        public static final int next=0x7f020009;
+        public static final int prev=0x7f02000a;
+        public static final int start=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int appstart=0x7f08000e;
-        public static final int btn_check=0x7f08000c;
-        public static final int btnnext=0x7f08000d;
-        public static final int btnprevious=0x7f08000b;
-        public static final int imageButton1=0x7f080001;
-        public static final int menu_settings=0x7f08000f;
-        public static final int placeHolderExplanation=0x7f080009;
-        public static final int placeHolderQuestion=0x7f080003;
-        public static final int placeHolderText=0x7f080005;
-        public static final int radioOption=0x7f080007;
-        public static final int rowButton=0x7f08000a;
-        public static final int rowEdit=0x7f080004;
-        public static final int rowExplanation=0x7f080008;
-        public static final int rowOption=0x7f080006;
-        public static final int rowQuestion=0x7f080002;
-        public static final int webinfo=0x7f080000;
+        public static final int aboutUs=0x7f090011;
+        public static final int action_settings=0x7f090010;
+        public static final int appstart=0x7f09000d;
+        public static final int btn_check=0x7f09000b;
+        public static final int btnnext=0x7f09000c;
+        public static final int btnprevious=0x7f09000a;
+        public static final int imageButton1=0x7f090001;
+        public static final int level1XmlFilePath=0x7f09000e;
+        public static final int menu_settings=0x7f09000f;
+        public static final int placeHolderExplanation=0x7f090008;
+        public static final int placeHolderText=0x7f090004;
+        public static final int preference=0x7f090012;
+        public static final int radioOption=0x7f090006;
+        public static final int rowButton=0x7f090009;
+        public static final int rowEdit=0x7f090003;
+        public static final int rowExplanation=0x7f090007;
+        public static final int rowOption=0x7f090005;
+        public static final int rowQuestion=0x7f090002;
+        public static final int webinfo=0x7f090000;
     }
     public static final class layout {
         public static final int activity_info=0x7f030000;
@@ -52,19 +61,19 @@ public final class R {
         public static final int activity_start=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_info=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int activity_info=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int btn_check=0x7f050008;
-        public static final int btn_next=0x7f050006;
-        public static final int btn_previous=0x7f050007;
-        public static final int hello_world=0x7f050002;
-        public static final int menu_settings=0x7f050003;
-        public static final int start_app=0x7f050005;
-        public static final int title_activity_info=0x7f050004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int btn_check=0x7f060008;
+        public static final int btn_next=0x7f060006;
+        public static final int btn_previous=0x7f060007;
+        public static final int hello_world=0x7f060002;
+        public static final int menu_settings=0x7f060003;
+        public static final int start_app=0x7f060005;
+        public static final int title_activity_info=0x7f060004;
     }
     public static final class style {
         /** 
@@ -88,10 +97,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
--- MyFirstApp/src/com/ajit/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/ajit/myfirstapp/MainActivity.java
@@ -3,22 +3,33 @@ package com.ajit.myfirstapp;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.ajit.myfirstapp.vo.QuestionAnswerVo;
@@ -28,26 +39,27 @@ public class MainActivity extends Activity  implements OnClickListener {
 	  private RadioGroup radioOptions;
 	  private TextView txtViewExplanation;
 	  private TextView txtViewText;
-	  private TextView txtViewQuestion;
 	  private Button btnDisplay;
 	  private ImageButton btnprevious, btnnext;
 	  private int position = 0;
 	  private List<QuestionAnswerVo> qaList = new ArrayList<QuestionAnswerVo>();
+	  private HashMap<String, Integer> map;
 	  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadImageInMap();
+        
         this.qaList = getQuestionAnswerList();
         
-        System.out.println(this.qaList + " - ");
-        System.out.println(this.qaList.size() + " - ");
+        //System.out.println(this.qaList + " - ");
+        //System.out.println(this.qaList.size() + " - ");
         if(this.qaList == null || this.qaList.size() < 1){
         	return;
         }
         
-        this.txtViewQuestion = (TextView)findViewById(R.id.placeHolderQuestion);
         this.txtViewExplanation = (TextView) findViewById(R.id.placeHolderExplanation);
         this.radioOptions = (RadioGroup)findViewById(R.id.radioOption);
         this.txtViewText = (TextView) findViewById(R.id.placeHolderText);
@@ -74,7 +86,20 @@ public class MainActivity extends Activity  implements OnClickListener {
         
     }
 
-    private List<QuestionAnswerVo> getQuestionAnswerList() {
+    private void loadImageInMap() {
+    	HashMap<String, Integer> images = new HashMap<String, Integer>();
+        images.put( "flower.jpg", Integer.valueOf( R.drawable.flower ) );
+        images.put( "flower_blue.jpg", Integer.valueOf( R.drawable.flower_blue ) );
+        images.put( "flower_red.jpg", Integer.valueOf( R.drawable.flower_red ) );
+        images.put( "flower_yellow.jpg", Integer.valueOf( R.drawable.flower_yellow ) );
+        images.put( "car_blue.jpg", Integer.valueOf( R.drawable.car_blue ) );
+        images.put( "car_red.jpg", Integer.valueOf( R.drawable.car_red ) );
+        images.put( "car_yellow.jpg", Integer.valueOf( R.drawable.car_yellow ) );
+        
+        this.map = images;
+	}
+
+	private List<QuestionAnswerVo> getQuestionAnswerList() {
     	List<QuestionAnswerVo> voList = null;
     	XmlPullParserFactory pullParserFactory;
 		try {
@@ -85,9 +110,9 @@ public class MainActivity extends Activity  implements OnClickListener {
 		        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 	            parser.setInput(in_s, null);
 
-	            System.out.println("step - 1");
+	            //System.out.println("step - 1");
 	            voList = parseXML(parser);
-	            System.out.println("step - 2");
+	            //System.out.println("step - 2");
 		} catch (XmlPullParserException e) {
 
 			e.printStackTrace();
@@ -103,7 +128,7 @@ public class MainActivity extends Activity  implements OnClickListener {
 		List<QuestionAnswerVo> voList = null;
         int eventType = parser.getEventType();
         QuestionAnswerVo questionAnswerVo = null;
-        System.out.println("eventType - " + eventType);
+        //System.out.println("eventType - " + eventType);
         
         while (eventType != XmlPullParser.END_DOCUMENT){
             String name = null;
@@ -111,17 +136,18 @@ public class MainActivity extends Activity  implements OnClickListener {
             switch (eventType){
                 case XmlPullParser.START_DOCUMENT:
                 	voList = new ArrayList<QuestionAnswerVo>();
-                	System.out.println("switch - 1");
+                	//System.out.println("switch - 1");
                     break;
                 case XmlPullParser.START_TAG:
                 	name = parser.getName();
-                	System.out.println("switch - 2 - " + name);
+                	//System.out.println("switch - 2 - " + name);
                     if (name.equals("quiz")){
                     	questionAnswerVo = new QuestionAnswerVo();
+                    	//System.out.println("questionAnswerVo : " + questionAnswerVo);
                     } else if (questionAnswerVo != null){
                         if (name.equals("question")){
                         	questionAnswerVo.question = parser.nextText();
-                        	System.out.println("questionAnswerVo.question - " + questionAnswerVo.question);
+                        	//System.out.println("questionAnswerVo.question - " + questionAnswerVo.question);
                         } else if (name.equals("answer")){
                         	questionAnswerVo.answer = parser.nextText();
                         } else if (name.equals("option1")){
@@ -139,14 +165,14 @@ public class MainActivity extends Activity  implements OnClickListener {
                     break;
                 case XmlPullParser.END_TAG:
                     name = parser.getName();
-                	System.out.println("switch - 3 - " + name);
+                	//System.out.println("switch - 3 - " + name);
                     if (name.equalsIgnoreCase("quiz") && questionAnswerVo != null){
                     	voList.add(questionAnswerVo);
                     } 
             }
             eventType = parser.next();
         }
-    	System.out.println("switch - 4 - " + voList.size());
+    	//System.out.println("switch - 4 - " + voList.size());
         return voList;
 	}
 
@@ -159,25 +185,33 @@ public class MainActivity extends Activity  implements OnClickListener {
     	this.radioOptions.removeAllViews();
     	
         RadioButton rdbtn = null;
-        rdbtn = new RadioButton(this);
-        rdbtn.setId(1);
-        rdbtn.setText(qaVo.option1);
-        this.radioOptions.addView(rdbtn);
+        if(qaVo.option1 != null && qaVo.option1 != ""){
+        	rdbtn = new RadioButton(this);
+            rdbtn.setId(1);
+            rdbtn.setText(qaVo.option1);
+            this.radioOptions.addView(rdbtn);	
+        }
         
-        rdbtn = new RadioButton(this);
-        rdbtn.setId(2);
-        rdbtn.setText(qaVo.option2);
-        this.radioOptions.addView(rdbtn);
+        if(qaVo.option2 != null && qaVo.option2 != ""){
+	        rdbtn = new RadioButton(this);
+	        rdbtn.setId(2);
+	        rdbtn.setText(qaVo.option2);
+	        this.radioOptions.addView(rdbtn);
+        }
 
+        if(qaVo.option3 != null && qaVo.option3 != ""){
         rdbtn = new RadioButton(this);
         rdbtn.setId(3);
         rdbtn.setText(qaVo.option3);
         this.radioOptions.addView(rdbtn);
+        }
         
+        if(qaVo.option4 != null && qaVo.option4 != ""){
         rdbtn = new RadioButton(this);
         rdbtn.setId(4);
         rdbtn.setText(qaVo.option4);
         this.radioOptions.addView(rdbtn);
+        }
 
    }
     
@@ -217,7 +251,7 @@ public class MainActivity extends Activity  implements OnClickListener {
 				int selectedId = radioOptions.getCheckedRadioButtonId();
 				
 				if(!(selectedId <= 0)){
-					System.out.println(qaVo.answer + " == " + selectedId);
+					//System.out.println(qaVo.answer + " == " + selectedId);
 					if(qaVo.answer.equals(""+selectedId)){
 						radioBtn = (RadioButton) findViewById(selectedId);
 						radioBtn.setBackgroundColor(Color.GREEN);
@@ -243,15 +277,13 @@ public class MainActivity extends Activity  implements OnClickListener {
     private void changePosition(int position){
     	QuestionAnswerVo qaVo = qaList.get(position);
 		
-		this.txtViewQuestion.setText(qaVo.question);
-		/*this.radioOptions.setVisibility(RadioGroup.INVISIBLE);
-		this.txtViewExplanation.setVisibility(TextView.INVISIBLE);
-		this.txtViewText.setVisibility(TextView.INVISIBLE);*/
+    	printQuestion(qaVo.question);
+		
 		this.radioOptions.setVisibility(View.GONE);
 		this.txtViewExplanation.setVisibility(View.GONE);
 		this.txtViewText.setVisibility(View.GONE);
 		
-		System.out.println("qaVo.option1 : " + qaVo.option1);
+		//System.out.println("qaVo.option1 : " + qaVo.option1);
 		if(qaVo.option1 == null || qaVo.option1.equals("")){
 			this.txtViewText.setText("");
 			this.txtViewText.setVisibility(TextView.VISIBLE);
@@ -264,6 +296,133 @@ public class MainActivity extends Activity  implements OnClickListener {
         
         
     }//end changeNumber
+
+    private void printQuestion(String question) {
+		String rawQuestion = question;
+
+		String strImage = "_s-img_";
+		String strImageNameCountSeparator = "-";
+
+		// get the screen size
+		DisplayMetrics mDisplayMetrics = this.getResources().getDisplayMetrics();
+		TableRow questiontr =  (TableRow)findViewById(R.id.rowQuestion);
+		questiontr.removeAllViews();
+		
+		TableLayout tblayout = new TableLayout(this);
+		
+		if(rawQuestion.indexOf(strImage) == -1){
+			System.out.println("IMage not found");
+			createQuestionTextView(rawQuestion, tblayout, mDisplayMetrics);			
+		}else{
+			 //_s-img_flower_blue.jpg,1_e-img_  _s-img_flower_red.jpg,2_e-img_ _s-img_car_red.jpg,1_e-img_
+			System.out.println("Image Found");
+			createQuestionView(rawQuestion, tblayout, mDisplayMetrics);
+			
+		}
+
+		
+		
+		
+		questiontr.addView(tblayout);
+	}
+
+	private void createQuestionTextView(String rawQuestion, TableLayout tblayout, DisplayMetrics mDisplayMetrics) {
+		TableRow row1 = new TableRow(this);
+		TextView qTextView = new TextView(this);
+		qTextView.setText(rawQuestion);
+		// force view width to only be as wide as the screen
+		qTextView.setMaxWidth(mDisplayMetrics.widthPixels);
+		row1.addView(qTextView);
+		tblayout.addView(row1);
+	}
+
+	private void createQuestionView(String rawQuestion, TableLayout tblayout, DisplayMetrics mDisplayMetrics) {
+		String strImage = "_s-img_";
+		String[] splittedImageString = rawQuestion.split(strImage);
+	    System.out.println(splittedImageString.length);
+		for(String str : splittedImageString){
+			System.out.println(str);
+			String pattern = "(.*),(\\d)_e-img_(.*)";
+
+		      // Create a Pattern object
+		      Pattern r = Pattern.compile(pattern);
+
+		      // Now create matcher object.
+		      Matcher m = r.matcher(str);
+		      if (m.find( )) {
+		    	  System.out.println("ImageName: " + m.group(1) );
+			         System.out.println("ImageCount: " + m.group(2) );
+			         System.out.println("more text : " + m.group(3) );
+		    	  int imageCount = (new Integer(m.group(2))).intValue();
+		    	  String imageName = m.group(1);
+		    	  
+		    	  TableRow row2 = new TableRow(this);
+		    	  for(int i = 1; i <= imageCount ; i ++){
+		    		  createQuestionImageView(imageName, row2, mDisplayMetrics);
+		    	  }
+		    	  tblayout.addView(row2);
+		         
+		      } else {
+		         createQuestionTextView(str, tblayout, mDisplayMetrics);
+		      }
+		}
+	}
+
+	private void createQuestionImageView(String imageName,
+			TableRow row2, DisplayMetrics mDisplayMetrics) {
+		
+		ImageView imageView = new ImageView(this);
+		imageView.setImageBitmap(
+			    decodeSampledBitmapFromResource(getResources(), getImageId(this, imageName), 50, 50));
+		row2.addView(imageView);
+		
+		
+	}
+	
+	public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+	        int reqWidth, int reqHeight) {
+
+	    // First decode with inJustDecodeBounds=true to check dimensions
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    BitmapFactory.decodeResource(res, resId, options);
+
+	    // Calculate inSampleSize
+	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+	    // Decode bitmap with inSampleSize set
+	    options.inJustDecodeBounds = false;
+	    return BitmapFactory.decodeResource(res, resId, options);
+	}
+	
+	public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		    // Raw height and width of image
+		    final int height = options.outHeight;
+		    final int width = options.outWidth;
+		    int inSampleSize = 1;
+		
+		    if (height > reqHeight || width > reqWidth) {
+		
+		        final int halfHeight = height / 2;
+		        final int halfWidth = width / 2;
+		
+		        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+		        // height and width larger than the requested height and width.
+		        while ((halfHeight / inSampleSize) > reqHeight
+		                && (halfWidth / inSampleSize) > reqWidth) {
+		            inSampleSize *= 2;
+		        }
+		    }
+		
+		    return inSampleSize;
+	}
     
+	public int getImageId(Context context, String imageName) {
+	    //int imageId =  context.getResources().getIdentifier(imageName, "drawable", context.getPackageName());
+		int imageId =  (this.map.get(imageName)).intValue();
+	    System.out.println("Image Id : " + imageId);
+	    return imageId;
+	}
 }
 
