331508946d2e635c3b1d03ec4c0ca53d871763db
ajitpandey <ajitpandey@outlook.com>
2014-02-18 20:20:57 +0000

    updated

---
+++ MyFirstApp/src/com/ajit/myfirstapp/AboutUs.java
@@ -0,0 +1,28 @@
+package com.ajit.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+
+public class AboutUs extends Activity {
+	private WebView webinfo;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+        
+        findViewById(R.id.btnAboutUsBack).setOnClickListener(btnBack_OnClickListener);
+    }
+    
+  //On click listener for btnLevel1
+    final OnClickListener btnBack_OnClickListener = new OnClickListener() {
+        public void onClick(final View v) {
+        	//invoke the Info activity
+				Intent main = new Intent(getApplicationContext(), Info.class);
+	    		startActivity(main);        
+        }
+    };
+}
