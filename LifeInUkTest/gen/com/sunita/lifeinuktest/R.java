3a14c64eb95acaf7740a6a46a5ef483c858ff07f
ajitpandey <ajitpandey@outlook.com>
2014-03-05 21:17:03 +0000

    updated

--- LifeInUkTest/gen/com/sunita/lifeinuktest/R.java
+++ LifeInUkTest/gen/com/sunita/lifeinuktest/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int aj=0x7f020000;
         public static final int aj_flashcard=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int next=0x7f020003;
-        public static final int prev=0x7f020004;
-        public static final int start=0x7f020005;
+        public static final int icon=0x7f020003;
+        public static final int icon_2=0x7f020004;
+        public static final int next=0x7f020005;
+        public static final int prev=0x7f020006;
+        public static final int start=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f090041;
